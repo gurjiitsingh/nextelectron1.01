85f3274f1e850ae7266ed47b424e1654f8a63f5c
Gurjit <gurjiitsingh@gmail.com>
2026-08-21 13:58:55 +0530

    update

--- src/app/(auth)/auth/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import RegisterForm from './RegisterForm';
-
-const Page = () => {
-  return (
-    <RegisterForm />
-  )
-}
-
-export default Page
--- src/app/(auth)/reset-password/page.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Suspense } from 'react'
-import ResetPasswordPage from './components/ResetPasswordPage'
-
-export default function page() {
-  return (
-    <Suspense> <ResetPasswordPage /></Suspense>
-   
-  )
-}
